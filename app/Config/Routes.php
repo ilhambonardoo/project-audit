049ec7df5563769f5c6908a1fedb78e8fdc8bb85
ilhambonardoo <ilhambonardo123@gmail.com>
2026-04-16 16:32:12 +0700

    mengatur routes agar lebih aman untuk hak akses role

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,20 +5,29 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
 $routes->get('/', 'Home::index');
-
-$routes->group('temuan', function($routes) {
-    $routes->get('/', 'Temuan::index');
-    $routes->get('create', 'Temuan::create');
-    $routes->post('store', 'Temuan::store');
-    $routes->get('show/(:num)', 'Temuan::show/$1');
-    $routes->post('verify', 'Temuan::verify');
-    $routes->get('edit/(:num)', 'Temuan::edit/$1');
-    $routes->post('update/(:num)', 'Temuan::update/$1');
-    $routes->get('delete/(:num)', 'Temuan::delete/$1');
-    $routes->get('/dashboard', 'Dashboard::index');
-});
-
 $routes->get('/login', 'Auth::index');
 $routes->post('/auth/loginProcess', 'Auth::loginProcess');
 $routes->get('/logout', 'Auth::logout');
+
+$routes->get('/dashboard', 'Dashboard::index');
+
+$routes->group('temuan', function($routes) {
+    $routes->get('/', 'Temuan::index');
+    $routes->get('show/(:num)', 'Temuan::show/$1');
+    
+    $routes->group('', ['filter' => 'role_id:1'], function($routes) {
+        $routes->get('create', 'Temuan::create');
+        $routes->post('store', 'Temuan::store');
+        $routes->get('edit/(:num)', 'Temuan::edit/$1');
+        $routes->post('update/(:num)', 'Temuan::update/$1');
+        $routes->get('delete/(:num)', 'Temuan::delete/$1');
+        $routes->post('verify', 'Temuan::verify');
+    });
+});
+
+$routes->group('tindak-lanjut', ['filter' => 'role_id:2'], function($routes) {
+    $routes->get('create/(:num)', 'TindakLanjut::create/$1');
+    $routes->post('store', 'TindakLanjut::store');
+});
